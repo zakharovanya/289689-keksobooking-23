45170ea7534bd02aa68dfa61994f19bfd74f6b98
Anna <zakharova.anya@list.ru>
2021-06-01 19:47:28 +0300

    Вторые правки

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-const getRandomInteget = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const randomInteget = Math.floor(Math.random() * (max - min + 1)) + min;
-  return Math.abs(randomInteget);
-};
-
 const getRandomNumber = function (min = 0, max = 10, precision = 3) {
-  const randomNumber = (Math.random() * (max - min + 1) + min).toFixed(Math.abs(precision));
-  return Math.abs(randomNumber);
+  return (Math.random() * (max - min + 1) + min).toFixed(Math.abs(precision));
 };
 
-getRandomInteget();
+const getRandomInteger = function (min = 0, max = 10) {
+  return getRandomNumber(min, max, 0);
+};
+
+getRandomInteger();
 getRandomNumber();
