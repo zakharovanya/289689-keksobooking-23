a50bec426cb91fbdc681cf7f18e593b34284fb04
Anna <zakharova.anya@list.ru>
2021-06-16 16:30:28 +0300

    правки модуляции

---
+++ js/mocks.js
@@ -0,0 +1,32 @@
+const OFFER_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const SIMILAR_OFFERS_COUNT = 10;
+
+export {OFFER_TYPES, CHECKINS, FEATURES, PHOTOS, SIMILAR_OFFERS_COUNT};
