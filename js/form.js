a49a8f186a7a25ac146488e5dac615ba23795e38
Anna <zakharova.anya@list.ru>
2021-07-02 15:17:58 +0300

    правки валидации

--- js/form.js
+++ js/form.js
@@ -1,17 +1,26 @@
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
-
-const minPriceOfType = {
-  bungalow: 0,
-  flat: 1000,
-  hotel: 3000,
-  house: 5000,
-  palace: 10000,
+const MAX_PRICE_LENGHT = 1000000;
+const MinPriceOfType = {
+  BUNGALOW: 0,
+  FLAT: 1000,
+  HOTEL: 3000,
+  HOUSE: 5000,
+  PALACE: 10000,
 };
 
-const MAX_PRICE_LENGHT = 1000000;
-
 const offerTitle = document.querySelector('#title');
+const offerPrice = document.querySelector('#price');
+const roomsSelect = document.querySelector('#room__number');
+
+const generateError = (text) => {
+  const error = document.createElement('div');
+  error.className = 'error';
+  error.style.color = 'red';
+  error.innerHTML = text;
+  return error;
+};
+
 offerTitle.addEventListener('input', () => {
   const valueTitleLength = offerTitle.value.length;
 
@@ -25,15 +34,18 @@ offerTitle.addEventListener('input', () => {
   offerTitle.reportValidity();
 });
 
-const offerPrice = document.querySelector('#price');
 offerPrice.addEventListener('input', () => {
-  const valuePriceLength = offerPrice.value.length;
-  for (const type in minPriceOfType) {
-    offerPrice.getElementById('price').placeholder = minPriceOfType[type];
-    if (valuePriceLength < minPriceOfType[type]) {
-      offerPrice.setCustomValidity(`Ещё ${  (minPriceOfType[type]) - valuePriceLength } симв.`);
-    } else if (valuePriceLength > MAX_PRICE_LENGHT) {
-      offerPrice.setCustomValidity(`Уменьшите цену на ${  valuePriceLength - MAX_PRICE_LENGHT } симв.`);
+  for (const type in MinPriceOfType) {
+    offerPrice.getElementById('price').placeholder = MinPriceOfType[type];
+    if (!offerPrice.value) {
+      const error = generateError('Цена не указана');
+      offerPrice.parentElement.insertBefore(error, offerPrice);
+    } else if (offerPrice.value < MinPriceOfType[type]) {
+      const error = generateError(`Цена должна быть выше ${(MinPriceOfType[type])}`);
+      offerPrice.parentElement.insertBefore(error, offerPrice);
+    } else if (offerPrice.value < MAX_PRICE_LENGHT) {
+      const error = generateError(`Цена должна быть ниже ${MAX_PRICE_LENGHT}`);
+      offerPrice.parentElement.insertBefore(error, offerPrice);
     } else {
       offerPrice.setCustomValidity('');
     }
@@ -41,30 +53,19 @@ offerPrice.addEventListener('input', () => {
   offerPrice.reportValidity();
 });
 
-const roomsSelect = document.querySelector('#room__number');
 roomsSelect.addEventListener('input', () => {
   const roomValue = roomsSelect.querySelectorAll('option').value;
   const capacitySelect = document.querySelector('#capacity');
-  const capacityOption = capacitySelect.querySelectorAll('option');
-  capacityOption.disabled = true;
-  if (roomValue === 1) {
-    capacityOption.value = 1;
-    capacityOption.disabled = false;
-    roomsSelect.setCustomValidity('Для 1 гостя');
-  } else if (roomValue === 2) {
-    capacityOption.value = 1 + 2;
-    capacityOption.disabled = false;
-    roomsSelect.setCustomValidity('Для 2 гостей или для 1 гостя');
-  } else if (roomValue === 3) {
-    capacityOption.value = 1 + 2 + 3;
-    capacityOption.disabled = false;
-    roomsSelect.setCustomValidity('Для 3 гостей», «для 2 гостей» или «для 1 гостя');
-  } else if (roomValue === 100) {
-    capacityOption.value = 0;
-    capacityOption.disabled = false;
-    roomsSelect.setCustomValidity('Не для гостей');
-  } else {
-    offerPrice.setCustomValidity('');
+  const capacityValue = capacitySelect.querySelectorAll('option').value;
+  capacityValue.disabled = true;
+  for (const i = 0; i <= 100;) {
+    if (roomValue[i] <= capacityValue[i]) {
+      capacityValue.disabled = false;
+    } else {
+      const error = generateError('Число гостей не должно превышать число комнат');
+      roomValue.parentElement.insertBefore(error, roomValue);
+    }
+    roomValue[i].remove();
   }
   roomsSelect.reportValidity();
 });
