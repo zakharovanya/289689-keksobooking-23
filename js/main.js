af2644cdf459bf7e53892214645185f5974f6bae
Anna <zakharova.anya@list.ru>
2021-07-04 15:02:41 +0300

    состояния формы и фильтра

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import {showPopup} from './popup.js';
 import {generateOffer} from './mocks.js';
-import './form.js';
+import {getActiveForm, getActiveFilter} from './form.js';
 
 const OFFERS_COUNT = 10;
 
 const offers = new Array(OFFERS_COUNT).fill(null).map(generateOffer);
 showPopup(offers[0]);
+
+getActiveForm();
+getActiveFilter();
