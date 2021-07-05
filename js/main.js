1ad1d3f7b5968f2811e0da70aa1753aa68a302e6
Anna <zakharova.anya@list.ru>
2021-07-05 23:54:12 +0300

    стыдные правки

--- js/main.js
+++ js/main.js
@@ -7,5 +7,5 @@ const OFFERS_COUNT = 10;
 
 const offers = new Array(OFFERS_COUNT).fill(null).map(generateOffer);
 showPopup(offers[0]);
-setAdFormEnabled(0);
-setMapFilterEnabled(0);
+setAdFormEnabled(false);
+setMapFilterEnabled(false);
